7e1ea405ff4a1aa7a3c575be88547ae73d1fb4b5
Ning Ning Sun <nsun200@live.com>
2018-06-06 17:56:08 -0400

    Fix error with bind

--- listen.py
+++ listen.py
@@ -4,7 +4,7 @@ port = 9999
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(socket.gethostname(), 9999)
+    server_socket.bind((socket.gethostname(), 9999))
     server_socket.listen(25)
     conn, addr = server_socket.accept()
     print(conn.recv(1000))
