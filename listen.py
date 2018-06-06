28bba7dbd9bd74b44cbe7ac8b56a28d812837fc1
Ning Ning Sun <nsun200@live.com>
2018-06-06 17:59:19 -0400

    get IP address

--- listen.py
+++ listen.py
@@ -2,9 +2,20 @@ import socket
 
 port = 9999
 
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
 def main():
+    ip = get_ip()
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((socket.gethostname(), 9999))
+    server_socket.bind((ip, 9999))
     server_socket.listen(25)
     conn, addr = server_socket.accept()
     print(conn.recv(1000))
