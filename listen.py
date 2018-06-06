3987509fada87fcdee79ae4d1610d945e51658b9
Ning Ning Sun <nsun200@live.com>
2018-06-06 18:36:47 -0400

    Add file transfer

--- listen.py
+++ listen.py
@@ -2,7 +2,6 @@ import socket
 
 port = 9999
 
-
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -10,15 +9,37 @@ def get_ip():
     s.close()
     return ip
 
-
 def main():
     ip = get_ip()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((ip, 9999))
+    server_socket.bind((ip, port))
     server_socket.listen(25)
-    conn, addr = server_socket.accept()
-    print(conn.recv(1000))
+    while True:
+        conn, addr = server_socket.accept()
+        files = conn.recv(4096).split(',')
+        ConnectionThread(conn, addr, files).start()
+
+
+
+class ConnectionThread(threading.Thread):
+    def __init__(self, conn, addr, files):
+        super().__init__()
+        self.client_socket = conn
+        self.files = files
+        self.addr = addr
+
+    def run(self):
+        for file in files:
+            with open(file, 'rb') as f:
+                buffer = f.read()
+                self.client_socket.send(buffer)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
